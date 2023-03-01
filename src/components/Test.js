c27e57413b97ec13f86e66eca66642e116b9f24b
ammarrqureshi <mamqureshi786@gmail.com>
2023-03-01 19:50:04 +0500

    having fun, domain first update

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Test = () => {
   return (
-    <div>Test
+    <div>
+      <center>
+        <h1>Welcome to PostDost </h1>
+      <p style={{ color: "grey", }}>Qureshi Companies Ltd.</p>
+      <p style={{ color: "grey", }}>CEO: SIDRA SHEIKH</p>
 
-        <p>This is a test for deployment</p>
-        <p>This is a test 2 for deployment</p>
-
+      </center>
+      
     </div>
+  );
+};
 
-  )
-}
-
-export default Test
+export default Test;
